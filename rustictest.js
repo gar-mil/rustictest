2039fadaa533e6dbbed292dae25b0fef55c3dcdc
Gareth Miller <6932794+gar-mil@users.noreply.github.com>
2024-01-08 11:13:23 -0600

    Fixed typo in comment

--- rustictest.js
+++ rustictest.js
@@ -8,7 +8,7 @@
  * Usage:
  * Step 1.) Instantiate the RusticTest class (var tests = new RusticTest()).
  * Step 2.) Run your tests (tests.assertEquals('1+1=2',2,'1+1')).
- * Step 3.) Display the results (tests.testResults())
+ * Step 3.) Display the results (tests.testResults()).
  */
 export class RusticTest
 {
